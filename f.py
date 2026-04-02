f7d816b80c93c436dc8458919a2e1e97860c53f7
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-04-02 09:28:23 +0530

    Add files via upload

--- f.py
+++ f.py
@@ -1,6 +1,10 @@
-x = 0b11 + 0b101j 
-y = 0b11 + 0x101j 
-z = 0b11 + 0o101j 
+x = "mohan is a "good" boy"
+
+print(x)
 
 
-# SyntaxError: invalid binary literal
+
+#   File "D:\pyprac\2_constants\4_str_constants\F.py", line 1
+#     x = "mohan is a "good" boy"
+#                      ^^^^
+# SyntaxError: invalid syntax
