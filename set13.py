38b9a0103666e63e7627e73b73bc2fc9a21cd413
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-04-30 23:56:28 +0530

    Add files via upload

---
+++ set13.py
@@ -0,0 +1,12 @@
+# set is not an indexed based collection..
+# sets are not subscriptable...
+# set is a hashbased collection....
+
+x = {67, 45, 32, 79}
+
+print(x[0])
+print(x[1])
+print(x[2])
+print(x[3])
+
+# TypeError: 'set' object is not subscriptable
