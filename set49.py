8ccb50dbd741a1a474ee349f1c6b38c90ee3aa1c
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-05-19 00:35:00 +0530

    Add files via upload

---
+++ set49.py
@@ -0,0 +1,16 @@
+x = {11, 12, 13, 14}
+y = {3, 5, 11, 6, 14}
+
+print(x, id(x))
+print(y, id(y))
+
+# union
+# z = x | y 
+z = x.union(y)
+
+print('##############')
+
+print(x, id(x))
+print(y, id(y))
+print('~~~~~~~~~~~~~~~~')
+print(z, id(z))
