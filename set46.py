9cbc3b9324a7fad311cf80abcc380f6f79d663b1
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-05-18 11:24:45 +0530

    Add files via upload

---
+++ set46.py
@@ -0,0 +1,15 @@
+x = {11, 12, 13, 14}
+y = {3, 5, 11, 6, 14}
+
+print(x, id(x))
+print(y, id(y))
+
+# intersection
+z = x & y 
+
+print('##############')
+
+print(x, id(x))
+print(y, id(y))
+print('~~~~~~~~~~~~~~~~')
+print(z, id(z))
