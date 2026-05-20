e74f648a4efd0e80b7c639162a8395e49d256a3c
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-05-20 23:20:33 +0530

    Add files via upload

---
+++ set52.py
@@ -0,0 +1,16 @@
+x = {11, 12, 13, 14}
+y = {3, 5, 11, 6, 14}
+
+print(x, id(x))
+print(y, id(y))
+
+# symmetric-difference
+# z = x ^ y 
+z = x.symmetric_difference(y)
+
+print('##############')
+
+print(x, id(x))
+print(y, id(y))
+print('~~~~~~~~~~~~~~~~')
+print(z, id(z))
