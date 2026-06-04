9c5ba884eca6a7c287f4f0fa1550c93301129bdd
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-06-04 09:45:04 +0530

    Add files via upload

---
+++ dic26.py
@@ -0,0 +1,12 @@
+# x = [('product_name', 'HP Laptop'), ('Price', 45000), ('Seller', 'MOhan Ent.')]
+# x = (['product_name', 'SSD 512GB'], ['Price', 9000], ['Seller', 'Reitesh Ent.'])
+
+
+# not ok
+# x = {['product_name', 'SSD 512GB'], ['Price', 9000], ['Seller', 'Reitesh Ent.']}
+
+print(x)
+
+y = dict(x)
+
+print(y)
