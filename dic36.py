6d6e442ffc68964e8b8a8b819903f7040b152935
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-06-09 10:29:42 +0530

    Add files via upload

---
+++ dic36.py
@@ -0,0 +1,10 @@
+x = {'name':'om', 'age': 23, 'college': 'global'}
+
+print(x, id(x))
+print('###################')
+print(x.popitem())
+print(x.popitem())
+print(x.popitem())
+print('###################')
+
+print(x, id(x))
