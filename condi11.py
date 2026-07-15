096e93defb0f0ac1e579308979de36975abc70ff
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-07-15 10:20:53 +0530

    Add files via upload

---
+++ condi11.py
@@ -0,0 +1,8 @@
+marks = eval(input('Enter your marks(%): '))
+
+x = 'Distinction' if marks >= 75 else \
+    '1st Division' if marks >= 60 else \
+    '2nd Division' if marks >= 45 else \
+    '3rd Division' if marks >= 33 else 'Fail'
+
+print(x)
