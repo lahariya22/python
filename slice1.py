aff5a29533cf336a950c348f6a4a2ed89038b3b5
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-07-27 16:20:17 +0530

    Add files via upload

---
+++ slice1.py
@@ -0,0 +1,12 @@
+x = [5, 7, 8, 9, 1, 0, 3, 4, 7]
+
+print(x)
+
+# <list>[<start-index> : <end-index>]
+# start-index: inclusive
+# end-index: exclusive
+y = x[2:7]
+print('~~~~~~~~~~~')
+
+print(y)
+print(x)
