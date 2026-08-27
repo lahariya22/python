2b0f0b69b0d9c55e6c9e24bfeb8273a2c6f77ab9
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-08-27 12:13:22 +0530

    Add files via upload

---
+++ if4.py
@@ -0,0 +1,14 @@
+print('boil water')
+
+x = input('Do you want sugar(y/n): ')
+
+if x == 'y':
+    print('add sugar')
+else:
+    print('add sugar free')
+
+print('add tea leaves')
+
+print('add milk')
+
+print('pour it into a cup')
