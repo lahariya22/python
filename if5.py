4e8c4f92c10cfb21f23a3b1f8c8c3525075c130d
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-08-27 12:13:43 +0530

    Add files via upload

---
+++ if5.py
@@ -0,0 +1,23 @@
+print('boil water')
+
+x = int(input('press 1 for sugar\n' \
+                'press 2 for jaggery\n' \
+                'press 3 for cinnamon\n' \
+                'press 4 for honey: '))
+
+if x == 1:
+    print('add sugar')
+elif x == 2:
+    print('add jaggery')
+elif x == 3:
+    print('add cinnamon')
+elif x == 4:
+    print('add honey')
+else:
+    print('add sugar free')
+
+print('add tea leaves')
+
+print('add milk')
+
+print('pour it into a cup')
