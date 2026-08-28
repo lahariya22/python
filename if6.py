1773a601afe1c866f7c44dfb820d7d678ab5156e
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-08-28 18:26:26 +0530

    Add files via upload

---
+++ if6.py
@@ -0,0 +1,7 @@
+x = int(input('Enter first number: '))
+y = int(input('Enter second number: '))
+
+if x > y:
+    print(f'x: {x} is greater than y: {y}')
+else:
+    print(f'y: {y} is greater than x: {x}')
