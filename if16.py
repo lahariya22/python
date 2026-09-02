7abbf901df95e8da00871a8bb435677abacb6160
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-09-02 11:14:30 +0530

    Add files via upload

---
+++ if16.py
@@ -0,0 +1,20 @@
+g = input('Enter gender(m/f): ')
+a = int(input('Enter Age: '))
+
+if (g == 'm' or g == 'f') and a >= 1:
+    if g == 'f':
+        if a <= 22:
+            print('MB: 0 and RI: 4%')
+        elif a >= 60:
+            print('MB: 0 and RI: 12%')
+        else:
+            print('MB: 2000 and RI: 8%')
+    elif g == 'm':
+        if a <= 22:
+            print('MB: 500 and RI: 2%')
+        elif a >= 60:
+            print('MB: 2000 and RI: 11%')
+        else:
+            print('MB: 5000 and RI: 6%')
+else:
+    print('Invalid Input...')
