19a5030b89338bbc1754b456d8a559ae0a206915
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-09-14 11:35:26 +0530

    Add files via upload

---
+++ for9.py
@@ -0,0 +1,8 @@
+x = {'name':'raj', 'age':23, 'college':'global'}
+# KEY methods are used to print the keys of the dictionary
+y = x.keys()
+# values method is used to print the values of the dictionary
+y = x.values()
+#y = x.items()
+# items is used to print the key and value in the form of tuple
+print(y)
