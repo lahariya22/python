5706fdc243c5ff03c7eeca40fa70c68cf5fe79be
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-09-15 10:25:39 +0530

    Add files via upload

---
+++ for11.py
@@ -0,0 +1,8 @@
+x = {'name':'raj', 'age':23, 'college':'global'}
+
+y = x.items()
+# items is used to print the key and value in the form of tuple
+# in next 1 by 1 tuple is stored in next and wwe  print  next it give tuple with key and value
+
+for next in y:
+    print(next)
