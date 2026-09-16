8554cd1a24f7fdade1c4ab966675feafaeab2a12
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-09-16 16:50:56 +0530

    Add files via upload

---
+++ for12.py
@@ -0,0 +1,8 @@
+x = {'name':'raj', 'age':23, 'college':'global'}
+
+y = x.items()
+# in this we use 2 value  in for loop.
+# in this k store the key and v store the value of the key from the tuple which is in y 
+# and we print k and v it give key and value of the dictionary
+for k,v in y:
+    print(k, v)
