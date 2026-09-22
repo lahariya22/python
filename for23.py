c3afaf988f4b7fd5cd4e0a56f209402fb7445cd7
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-09-22 10:56:13 +0530

    Add files via upload

---
+++ for23.py
@@ -0,0 +1,11 @@
+list = [
+        ['chandragupt','bindusar','bimbisar','ashok'], 
+        {'yudhishthir','bheem','arjun','nakul','sahdev'},
+        ('Ram','Lakshman','Bharat', 'Shatrughna')
+        ]
+# in this we have a list of list, set and tuple and 
+# for loop will print each element of the list one by one.
+# it print set tuple list as a record of list and
+#  it will not print each element of set tuple list one by one.
+for each in list:
+    print(each)
