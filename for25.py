d3ec821391026293664551d0f2fd321aafdaa262
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-09-23 19:49:09 +0530

    Add files via upload

---
+++ for25.py
@@ -0,0 +1,15 @@
+x = [4, 7, 12, 9, 13, 8, 3, 6, 21, 37]
+
+odd_count = 0
+
+for each in x:
+    if each % 2 == 0:
+        print(each)
+    else:
+        odd_count += 1
+
+print('-----------------')
+print(f'odd count: {odd_count}')
+
+
+# X.py
