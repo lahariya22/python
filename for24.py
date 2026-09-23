8daea0fe4d6e01b46945e2538bacbff3714016ce
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-09-23 19:47:40 +0530

    Add files via upload

---
+++ for24.py
@@ -0,0 +1,11 @@
+list = [
+        ['chandragupt','bindusar','bimbisar','ashok'], 
+        {'yudhishthir','bheem','arjun','nakul','sahdev'},
+        ('Ram','Lakshman','Bharat', 'Shatrughna')
+        ]
+# it print list record items list
+
+for each in list:
+    for next in each:
+        print(next, end=' - ')
+    print()
