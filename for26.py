2aaf3b211a7b92c07b64814648c387a4caf0e85e
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-09-23 19:50:43 +0530

    Add files via upload

---
+++ for26.py
@@ -0,0 +1,9 @@
+x = [4, 7, 12, 9, 13, 8, 3, 6, 21, 37]
+
+for each in x:
+    if each % 2 == 0:
+        print('The list contains even record')
+    else:
+        print('The list does not contain even record')
+
+        
