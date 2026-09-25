721cbec37f44911b9c01db23774427f733eb0e11
Aarchi Lahariya <aarchilahariya@gmail.com>
2026-09-25 11:14:31 +0530

    Add files via upload

---
+++ for28.py
@@ -0,0 +1,13 @@
+x = [41, 7, 121, 9, 13, 81, 3, 61, 21, 37]
+# x = [41, 7, 12, 9, 13, 8, 3, 6, 21, 37]
+
+flag = False
+
+for each in x:
+    print('--------------------')
+    if each % 2 == 0:
+        flag = True 
+        break
+
+if flag:
+    print('The list contains even record')
